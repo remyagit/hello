e27d15da09a3bed02ae6e1f21670917b63c6e4d1
remya <nairkremya@gmail.com>
2021-04-04 20:21:53 -0400

    adding worlds

---
+++ src/hello/helloWorld.java
@@ -0,0 +1,17 @@
+package hello;
+
+
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//System.out.println("Hello World");
+
+
+String a="Hello worlds";
+System.out.println(a);
+
+	}
+
+}
